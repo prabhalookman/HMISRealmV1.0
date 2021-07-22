1df36ffbe2e820177a05862da30ad3d4f1eec8b9
Prabakaran <prabakaran@lookman.in>
2021-07-22 14:06:08 +0530

    initial commit

---
+++ model/index.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+import Site from './site';
+import Workspace from './workspace';
+import Location from './location';
+import LocationSetting from './locationsetting';
+import Business from './business';
+import BusinessInfo from './businessInfo';
+import EventCategory  from "./eventcategory"
+import Event  from "./events"
+import AddOn from "./addons"
+import Staff from "./staff"
+import StaffDetails from "./staffdetails"
+import Address from "./address"
+
+import Role  from './role'
+import User  from './user'
+import TimeFormat  from './timeformat'
+import Timings  from './timings'
+
+const connectMongo = () => {
+  return mongoose.connect(process.env.MONGO_URL, {useNewUrlParser : true, useFindAndModify: false, useUnifiedTopology: true})
+}
+
+const models = {
+  Site,
+  Workspace,
+  Location,
+  LocationSetting,
+  Business,
+  BusinessInfo,
+  EventCategory,
+  Event,
+  AddOn,
+  Role,
+  User,
+  TimeFormat,
+  Timings,
+  Staff,
+  StaffDetails,
+  Address
+}
+
+export {connectMongo};
+export default models;
---
+++ schema/site.js
@@ -0,0 +1,50 @@
+import { gql } from 'apollo-server-express';
+
+export default gql`
+
+type Site {
+    _id: ID
+    name: String
+    default_language: String
+    account_status: String
+    accessible: Boolean    
+    active: Boolean
+    suspended: Boolean
+    deleted: Boolean
+    payment_status_id: String
+    payment_status: String
+    subcription_id: ID
+    created_by: ID
+    created_at: String
+    updated_at: String
+    deleted_at: String
+    googleAnalyticsCode: String
+}
+input siteInput {
+    name: String
+    default_language: String
+    account_status: String
+    accessible: Boolean    
+    active: Boolean
+    suspended: Boolean
+    deleted: Boolean
+    payment_status_id: String
+    payment_status: String
+    subcription_id: ID
+    created_by: ID
+    created_at: String
+    updated_at: String
+    deleted_at: String
+    googleAnalyticsCode: String
+}
+
+extend type Query {
+    getSite: [Site]
+}
+
+extend type Mutation {
+    addSite(input: siteInput): Site
+    updateSite(siteID: ID!, input: siteInput): Site
+    deleteSite(siteID: ID!): Site
+}
+`
